c7d4222f8416037c4ead4c13448c57f85606012c
JP Doherty <jpdoherty90@gmail.com>
2017-06-24 15:20:53 -0500

    fixed some bugs, still needs polishing

--- algorithm.js
+++ algorithm.js
@@ -3,9 +3,9 @@ function countdown(days) {
     for(var i = days; i > 0; i--) {
         var msg1 = "days until my birthday.";
         var msg2 = "";
-        if (days > 30) {
+        if (i > 30) {
             msg2 = "Such a long time... :(";
-        } else if (days > 5) {
+        } else if (i > 5) {
             msg2 = "Getting closer!";
         } else {
             msg1 = msg1.toUpperCase();
